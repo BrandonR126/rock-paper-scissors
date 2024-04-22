efe609a44c92b7026d2d501fe78147630c1422dc
BrandonR126 <brandonrosen0126@gmail.com>
2024-04-21 21:01:24 -0400

    ready to work on

--- mainScript.js
+++ mainScript.js
@@ -101,5 +101,3 @@ submitButton.addEventListener('mouseup', function(event) {
     // Change the button style when it's pressed down
     submitButton.style.backgroundColor = "buttonface";
 });
-
-asdasd
