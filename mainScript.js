149b0a1ff3a7b3a8de9848c38d2b9ed77a1404d0
BrandonR126 <brandonrosen0126@gmail.com>
2024-04-22 20:01:42 -0400

    finished all logic and frontend

--- mainScript.js
+++ mainScript.js
@@ -5,17 +5,42 @@ const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 const submitButton = document.querySelector("#submit");
-const restartButton = document.querySelector("#restart");
 
 // Variables for js
 let computerInput
 let userInput
-let finalWinner
+let finalWinner = "undefined"
 let endFinal
 let computerGuess
 
 
-function main() {
+// Gets the users choice
+rockButton.onclick = () => {
+    userInput = "rock";
+    console.log(userInput + "user input");
+    rockButton.setAttribute("style", "background-color: grey;")
+    paperButton.setAttribute("style", "background-color: buttonface;")
+    scissorsButton.setAttribute("style", "background-color: buttonface;")
+};
+
+paperButton.onclick = () => {
+    userInput = "paper";
+    console.log(userInput + "user input");
+    rockButton.setAttribute("style", "background-color: buttonface;")
+    paperButton.setAttribute("style", "background-color: grey;")
+    scissorsButton.setAttribute("style", "background-color: buttonface;")
+};
+
+scissorsButton.onclick = () => {
+    userInput = "scissors";
+    console.log(userInput + "user input");
+    rockButton.setAttribute("style", "background-color: buttonface;")
+    paperButton.setAttribute("style", "background-color: buttonface;")
+    scissorsButton.setAttribute("style", "background-color: grey;")
+};
+
+
+function main(userInput) {
 
     // Get computers guess
     computerGuess = Math.floor((Math.random() * 3) + 1);
@@ -35,64 +60,36 @@ function main() {
     }
     console.log(computerInput);
 
-    // Gets the users choice
-    rockButton.onclick = () => {
-        userInput = "rock";
-        console.log(userInput + "user input");
-        rockButton.setAttribute("style", "background-color: grey;")
-        paperButton.setAttribute("style", "background-color: buttonface;")
-        scissorsButton.setAttribute("style", "background-color: buttonface;")
-    };
-
-    paperButton.onclick = () => {
-        userInput = "paper";
-        console.log(userInput + "user input");
-        rockButton.setAttribute("style", "background-color: buttonface;")
-        paperButton.setAttribute("style", "background-color: grey;")
-        scissorsButton.setAttribute("style", "background-color: buttonface;")
-    };
-
-    scissorsButton.onclick = () => {
-        userInput = "scissors";
-        console.log(userInput + "user input");
-        rockButton.setAttribute("style", "background-color: buttonface;")
-        paperButton.setAttribute("style", "background-color: buttonface;")
-        scissorsButton.setAttribute("style", "background-color: grey;")
-    };
-
     // Finds the winner
     if(userInput == computerInput) {
-        finalWinner = "You both chose ${userInput} .";
-    }
-    else {
-        finalWinner = "Error calculating results"
+        finalWinner = `You both chose ${userInput} .`;
+    } else if(userInput == "rock" & computerInput == "scissors") {
+        finalWinner = `You won! You chose ${userInput} and the computer chose ${computerInput} .`;
+    } else if(userInput == "paper" & computerInput == "rock") {
+        finalWinner = `You won! You chose ${userInput} and the computer chose ${computerInput} .`;
+    } else if(userInput == "scissors" & computerInput == "paper") {
+        finalWinner = `You won! You chose ${userInput} and the computer chose ${computerInput} .`;
+    } else if(finalWinner == "undefined" || finalWinner == "Error calculating results. You likely pressed submit without selecting an option, please make sure to select an option.") {
+        finalWinner = "Error calculating results. You likely pressed submit without selecting an option, please make sure to select an option.";
+    } else {
+        finalWinner = `You lost! You chose ${userInput} and the computer chose ${computerInput} .`;
     }
     return finalWinner
+
 }
 
-// Restart and submit buttons functionality
-restartButton.onclick = () => {
-    endFinal = main();
-    
-}
-
+//Submit button functionality
 submitButton.onclick = () => {
-    finalOutut = (endFinal);
-    console.clear();
+    endFinal = main(userInput);
+    winnerOutput.textContent = (endFinal);
     rockButton.setAttribute("style", "background-color: buttonface;")
     paperButton.setAttribute("style", "background-color: buttonface;")
     scissorsButton.setAttribute("style", "background-color: buttonface;")
+    console.log(finalWinner)
 };
 
-//Change colors of buttons when pressed
-restartButton.addEventListener('mousedown', function(event) {
-    // Change the button style when it's pressed down
-    restartButton.style.backgroundColor = "grey";
-});
-restartButton.addEventListener('mouseup', function(event) {
-    // Change the button style when it's pressed down
-    restartButton.style.backgroundColor = "buttonface";
-});
+
+//Change colors of submit button when pressed
 submitButton.addEventListener('mousedown', function(event) {
     // Change the button style when it's pressed down
     submitButton.style.backgroundColor = "grey";
